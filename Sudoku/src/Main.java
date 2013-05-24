761d338350f28db563a431d97c8137db0f6cc93b
David Chavigny <Gotan25@hotmail.com>
2013-05-24 13:55:40 -0400

    Pretty much done for the algo. Not tested.

--- Sudoku/src/Main.java
+++ Sudoku/src/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//Load file!
 		
+		//Sudoku.Solve(new Byte[][]); //Solves a col/line Sudoku double array.
 	}
 
 }
---
+++ Sudoku/src/Sudoku.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+
+
+
+public class Sudoku {
+	private static byte[][] _Sudoku;
+	
+	public static boolean Solve(byte[][] Sudoku)
+	{
+		_Sudoku = Sudoku;
+		return setNumber(0, 0);
+	}
+	
+	private static boolean setNumber(int col, int line)
+	{
+		//Find available numbers
+		Byte[] AvailableNumbers = getAvailableNumbers(col, line);
+		
+		//Find next coordinates
+		int nextCol = col;
+		int nextLine = line;
+		do //Used a do while, since we're always at least incrementing by one. 
+		{
+			nextCol = (++nextCol) % 9; //Switch to next column
+		if(nextCol == 0) //If we were at column 9, the next coordinates are [0][line+1]
+			nextLine++;
+		}
+		while(!isAvailable(nextCol, nextLine)); //Next case might not be empty, in which case it should be skipped! 
+		
+		
+		for(byte b : AvailableNumbers)
+		{
+			_Sudoku[col][line] = b; //Set the value
+			if(nextLine == 9) //We've reached the end, final number has been set! 
+				return true; 
+			return setNumber(nextCol, nextLine); //set the next one in line. 
+		}
+		return false;
+	}
+	
+	//Checks to see if the requested box doesn't already contain a number. 
+	private static boolean isAvailable(int col, int line)
+	{
+		return _Sudoku[col][line] == 0;
+	}
+	
+	private static Byte[] getAvailableNumbers(int col, int line)
+	{
+		Byte[] AvailableNumbers = new Byte[]{null,1,2,3,4,5,6,7,8,9}; //Available numbers. 
+		//Check line and col
+		for(int i = 0; i < 9; i++)
+		{
+			AvailableNumbers[_Sudoku[i][line]] = null; 
+			AvailableNumbers[_Sudoku[col][i]] = null;
+			//This one might be a bit hard to understand, so I'll give an example:
+			//say there's a Sudoku with 0 1 5 as the first 3 numbers of the first line
+			//First round, it'll remove [i][line] (IE: the first element of the list) from the array: position 0. null => null 
+			//Second round, it'll remove [i][line] from the array, position 1. 1 => null; the number is unavailable. 
+			//Third round, same logic; remove [5], so 5 => null. ETC ETC. Does this simultaneously for line and column. 
+		}
+		//Next is block. 
+		int startCol = (col / 3) * 3; //5 / 3 = 1, *3 = 3
+		int startLine = (line / 3) * 3;
+		for(int x = 0; x < 3; x++)
+			for(int y = 0; y < 3; y++)
+				AvailableNumbers[_Sudoku[x + startCol][y + startLine]] = null; //0,1,2 / 3,4,5 / 6,7,8, depending on startCol/startLine
+		
+		
+		return AvailableNumbers;
+	}
+	
+	
+}
