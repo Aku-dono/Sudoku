56c1aea1c583cd99d5b2f6c8c630c53ba37340cc
David Chavigny <Gotan25@hotmail.com>
2013-05-27 11:14:01 -0400

    Sudoku tested, works.

--- Sudoku/src/Main.java
+++ Sudoku/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Timer;
+
 
 public class Main {
 
@@ -6,9 +8,50 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		//Load file!
-		
-		
+
+		//Test array
+		byte[][] TestSudoku = new byte[][]
+				{
+				{0,3, 0,6, 0,5, 0, 0, 0},
+				{6, 0, 0, 0,9, 0, 0, 0,2},
+				{ 0,7, 0,1, 0, 0, 0, 0,6},
+				{ 0,9, 0, 0, 0, 0, 0, 0, 0},
+				{8,1, 0, 0,5, 0, 0,6,9},
+				{ 0, 0, 0, 0, 0, 0, 0,8, 0},
+				{4, 0, 0, 0, 0,3, 0,2, 0},
+				{9, 0, 0, 0,2, 0, 0, 0,5},
+				{ 0, 0, 0,9, 0,8, 0,3, 0},
+				};
+
+		System.out.println("Beginning solve.");
+		long startTime = System.nanoTime();
+				if(Sudoku.Solve(TestSudoku))
+		{
+			long endTime = System.nanoTime();
+			long duration = endTime - startTime;
+			System.out.println("Solution found in " + duration + " nanoseconds");
+
+			printSudoku(TestSudoku);
+		}
+		else
+			System.out.println("No solution found.");
+
 		//Sudoku.Solve(new Byte[][]); //Solves a col/line Sudoku double array.
+	}
+
+
+	private static void printSudoku(byte[][] Sudoku)
+	{
+		for(byte[] line : Sudoku)
+		{
+			System.out.print('[');
+			for(byte b : line)
+			{
+				System.out.print(b + " ");
+			}
+			System.out.print("]\n");
+			System.out.flush();
+		}
 	}
 
 }
--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -4,48 +4,61 @@ import java.util.ArrayList;
 
 
 public class Sudoku {
-	private static byte[][] _Sudoku;
-	
+	private static byte[][] _Sudoku; 
+
 	public static boolean Solve(byte[][] Sudoku)
 	{
-		_Sudoku = Sudoku;
+		_Sudoku = Sudoku; //Because this is a reference, it modifies the original object. So there's no need to access _Sudoku. 
 		return setNumber(0, 0);
 	}
-	
+
 	private static boolean setNumber(int col, int line)
 	{
 		//Find available numbers
 		Byte[] AvailableNumbers = getAvailableNumbers(col, line);
-		
-		
+
+
 		//Find next coordinates
 		int nextCol = col;
 		int nextLine = line;
-		do //Used a do while, since we're always at least incrementing by one. 
-		{
+
+		do{ //Used a do while, since we're always at least incrementing by one. 
 			nextCol = (++nextCol) % 9; //Switch to next column
-		if(nextCol == 0) //If we were at column 9, the next coordinates are [0][line+1]
-			nextLine++;
-		}
-		while(!isAvailable(nextCol, nextLine)); //Next case might not be empty, in which case it should be skipped! 
+			if(nextCol == 0) //If we were at column 9, the next coordinates are [0][line+1]
+				nextLine++;
+		} while(nextLine < 9 && !isAvailable(nextCol, nextLine)); //Next case might not be empty, in which case it should be skipped! 
 		
-		
-		for(byte b : AvailableNumbers)
+		//DEBUG
+		/*String strAvNum = "[";
+		for(Byte b : AvailableNumbers)
 		{
+			strAvNum += b + ", ";
+		}
+		strAvNum += "]";
+		System.out.println("Available numbers for " + col + ", " + line + ": " + strAvNum);*/
+		
+		
+		for(Byte b : AvailableNumbers)
+		{
+			if(b == null)
+				continue;
+			//System.out.println("testing " + b + " at " + "[" + col + "][" + line + "]");
 			_Sudoku[col][line] = b; //Set the value
 			if(nextLine == 9) //We've reached the end, final number has been set! 
 				return true; 
-			setNumber(nextCol, nextLine); //set the next one in line. 
+			if(setNumber(nextCol, nextLine))//set the next one in line.
+				return true;  //If setNumber managed to fit the last number in, go back all the way to the first with success. 
 		}
+		_Sudoku[col][line] = 0; //Backing up. 
 		return false;
 	}
-	
+
 	//Checks to see if the requested box doesn't already contain a number. 
 	private static boolean isAvailable(int col, int line)
 	{
 		return _Sudoku[col][line] == 0;
 	}
-	
+
 	private static Byte[] getAvailableNumbers(int col, int line)
 	{
 		Byte[] AvailableNumbers = new Byte[]{null,1,2,3,4,5,6,7,8,9}; //Available numbers. 
@@ -66,10 +79,10 @@ public class Sudoku {
 		for(int x = 0; x < 3; x++)
 			for(int y = 0; y < 3; y++)
 				AvailableNumbers[_Sudoku[x + startCol][y + startLine]] = null; //0,1,2 / 3,4,5 / 6,7,8, depending on startCol/startLine
-		
-		
+
+
 		return AvailableNumbers;
 	}
-	
-	
+
+
 }
